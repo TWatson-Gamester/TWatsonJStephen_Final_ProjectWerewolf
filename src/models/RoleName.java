c36b10b5c4a06f62a0a669074f3473ff1eabfd21
twatson <twatson@student.neumont.edu>
2021-02-27 13:13:46 -0700

    Added the Enum RoleName.java

---
+++ src/models/RoleName.java
@@ -0,0 +1,19 @@
+package models;
+
+public enum RoleName {
+    VILLAGER,
+    WEREWOLF,
+    SEER,
+    HUNTER,
+    BODYGUARD,
+    APPRENTICE_SEER,
+    CUPID,
+    WOLF_CUB,
+    TANNER,
+    CURSED,
+    LYCAN,
+    CULT_LEADER,
+    MASON,
+    LONE_WOLF,
+    WITCH
+}
