364332ecc74199b369ab332328d5723e162b02f2
jstephen <jstephen@student.neumont.edu>
2021-02-27 13:22:05 -0700

    Players get and set methods complete

--- src/models/Players.java
+++ src/models/Players.java
@@ -1,4 +1,80 @@
 package models;
 
 public class Players {
+
+    private int seatNumber;
+    private Roles currentRole;
+    private boolean isDead;
+    private boolean openGrave;
+    private boolean isVillage;
+    private boolean hasWon;
+    private boolean isLovers;
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public Roles getCurrentRole() {
+        return currentRole;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public boolean getOpenGrave() {
+        return openGrave;
+    }
+
+    public boolean isVillage() {
+        return isVillage;
+    }
+
+    public boolean hasWon() {
+        return hasWon;
+    }
+
+    public boolean isLovers() {
+        return isLovers;
+    }
+
+    //
+
+    public void setSeatNumber(int seatNumber) {
+        if(seatNumber > 0 && seatNumber < 21){
+        this.seatNumber = seatNumber;
+        }else{
+            throw new IllegalArgumentException("This is not a valid seat.");
+        }
+    }
+
+    public void setCurrentRole(Roles currentRole) {
+        if(currentRole != null) {
+            this.currentRole = currentRole;
+        }else{
+            throw new IllegalArgumentException("This role is not currently supported.");
+        }
+    }
+
+    public void setDead(boolean dead) {
+        isDead = dead;
+    }
+
+    public void setOpenGrave(boolean openGrave) {
+        this.openGrave = openGrave;
+    }
+
+    public void setVillage(boolean village) {
+        isVillage = village;
+    }
+
+    public void setWon(boolean hasWon) {
+        this.hasWon = hasWon;
+    }
+
+    public void setLovers(boolean lovers) {
+        isLovers = lovers;
+    }
+
+
 }
