1be40e0b3990b414ed76d886ea92f8080299a6bf
jstephen <jstephen@student.neumont.edu>
2021-03-01 13:39:34 -0700

    Main generateGame method and changes to GameController main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,49 @@
 package com.company;
 
 import controllers.GameController;
+import models.Players;
+import models.RoleName;
+import models.Roles;
+
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameController.runGame();
+        GameController.runGame(generateGame());
+    }
+
+
+    private static ArrayList<Players> generateGame(){
+        Roles villager = new Roles();
+        villager.setName(RoleName.VILLAGER);
+        Roles werewolf = new Roles();
+        werewolf.setName(RoleName.WEREWOLF);
+        Roles seer = new Roles();
+        seer.setName(RoleName.SEER);
+
+        Players villagerPlayer = new Players();
+        villagerPlayer.setCurrentRole(villager);
+        villagerPlayer.setDead(false);
+        villagerPlayer.setVillage(true);
+
+        Players werewolfPlayer = new Players();
+        werewolfPlayer.setCurrentRole(werewolf);
+        werewolfPlayer.setDead(false);
+        werewolfPlayer.setVillage(true);
+
+        Players seerPlayer = new Players();
+        seerPlayer.setCurrentRole(seer);
+        seerPlayer.setDead(false);
+        seerPlayer.setVillage(true);
+
+        originalCast.add(villagerPlayer);
+        originalCast.add(villagerPlayer);
+        originalCast.add(villagerPlayer);
+        originalCast.add(villagerPlayer);
+        originalCast.add(werewolfPlayer);
+        originalCast.add(seerPlayer);
+
+        return null;
     }
 }
--- src/controllers/GameController.java
+++ src/controllers/GameController.java
@@ -14,39 +14,10 @@ public class GameController {
     private static boolean isDay = false;
     private static int dayNumber = 0;
 
-    public static void runGame(){
+    public static void runGame(ArrayList<Players> players){
 
-        Roles villager = new Roles();
-        villager.setName(RoleName.VILLAGER);
-        Roles werewolf = new Roles();
-        werewolf.setName(RoleName.WEREWOLF);
-        Roles seer = new Roles();
-        seer.setName(RoleName.SEER);
-
-        Players villagerPlayer = new Players();
-        villagerPlayer.setCurrentRole(villager);
-        villagerPlayer.setDead(false);
-        villagerPlayer.setVillage(true);
-
-        Players werewolfPlayer = new Players();
-        werewolfPlayer.setCurrentRole(werewolf);
-        werewolfPlayer.setDead(false);
-        werewolfPlayer.setVillage(true);
-
-        Players seerPlayer = new Players();
-        seerPlayer.setCurrentRole(seer);
-        seerPlayer.setDead(false);
-        seerPlayer.setVillage(true);
-
-        originalCast.add(villagerPlayer);
-        originalCast.add(villagerPlayer);
-        originalCast.add(villagerPlayer);
-        originalCast.add(villagerPlayer);
-        originalCast.add(werewolfPlayer);
-        originalCast.add(seerPlayer);
-
-        villagePeople = originalCast;
-        dayTime();
+        originalCast = players;
+        villagePeople = players;
 
         do{
             if(isDay){
