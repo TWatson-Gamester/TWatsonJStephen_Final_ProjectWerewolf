720c52e96e2460f71f492ff8b9eb9d9985e07e87
twatson <twatson@student.neumont.edu>
2021-03-02 16:53:51 -0700

    Got rid of imports not in use in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,6 @@
 package com.company;
 
-import controllers.GameController;
 import controllers.WerewolfMainMenu;
-import models.Players;
-import models.RoleName;
-import models.Roles;
-
-import java.io.File;
-import java.util.ArrayList;
 
 public class Main {
 
